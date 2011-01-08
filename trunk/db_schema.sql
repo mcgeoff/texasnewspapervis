2c0b3728f5705c753609f0785de69071768538e4
yinfeng.qin@gmail.com <yinfeng.qin@gmail.com@b9205858-1e49-5449-e52a-7d3b763206c1>
2011-01-08 23:07:16 +0000

    added new version of data

--- trunk/db_schema.sql
+++ trunk/db_schema.sql
@@ -1,11 +1,14 @@
 
 /*
 drop table if exists newspaper_count;
-drop table if exists location;
-create table newspaper_count (id text, pub text, city text, state text, dateTime date, mNumber int, mCommon int, mProper int, mSymbol int, mUnknown int, mBad int, mTotal int);
-create table location (zip text, city text, county text, state text, longitude real, latitude real);
+create table newspaper_count (id text, pub text, city text, state text, dateTime date, mNumber int, mCommon int, mProper int, mSymbol int, mUnknown int, mBad int, mTotal int, pages int, wordsPerPage float);
 
-.import newspaper_count.txt newspaper_count;
+.import newspaper_count.txt newspaper_count
+*/
+
+/*
+drop table if exists location;
+create table location (zip text, city text, county text, state text, longitude real, latitude real);
 .import location.txt location;
 */
 
@@ -14,7 +17,9 @@ create table location (zip text, city text, county text, state text, longitude r
 select pub, city, state, strftime("%Y", dateTime), sum(mUnknown+mBad), sum(mTotal)
 from newspaper_count
 group by pub, city, state, strftime("%Y", dateTime);
+*/
 
+/*
 drop view if exists pub_by_year;
 drop view if exists city_by_year;
 
@@ -33,5 +38,4 @@ select city,
        sum(mTotal) as mTotal
 from newspaper_count
 group by city, strftime("%Y", dateTime);
-
 */
